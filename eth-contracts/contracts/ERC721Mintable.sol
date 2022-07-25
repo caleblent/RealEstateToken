326db729e777e2ba5af7cac74e4cdb75a7be9d29
Caleb Lent <caleb@stcroixwebagency.com>
2022-07-25 14:16:03 -0500

    Ownable{} contract in ERC721Mintable

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -8,17 +8,44 @@ import "./Oraclize.sol";
 
 contract Ownable {
     //  TODO's
-    //  1) create a private '_owner' variable of type address with a public getter function
-    //  2) create an internal constructor that sets the _owner var to the creater of the contract 
-    //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner.
-    //  4) fill out the transferOwnership function
+    //  1) create a private '_owner' variable of type address with a public getter function DONE
+    //  2) create an internal constructor that sets the _owner var to the creater of the contract DONE
+    //  3) create an 'onlyOwner' modifier that throws if called by any account other than the owner. DONE
+    //  4) fill out the transferOwnership function DONE
     //  5) create an event that emits anytime ownerShip is transfered (including in the constructor)
+
+    address private _owner;
+
+    function getOwner() public returns(address) {
+        return _owner;
+    }
+
+    constructor() internal {
+        this._owner = msg.sender;
+
+        emit OwnershipTransferred(address(0), msg.sender);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == this._owner, "Caller is not the owner");
+        _;
+    }
 
     function transferOwnership(address newOwner) public onlyOwner {
         // TODO add functionality to transfer control of the contract to a newOwner.
-        // make sure the new owner is a real address
+        address oldOwner = this._owner;
+        this._owner == newOwner;
 
+        // make sure the new owner is a real address
+        // HOW???
+        // I think this is handled with the 'onlyOwner' modifier
+
+        emit OwnershipTransferred(oldOwner, newOwner);
     }
+
+    event OwnershipTransferred(address oldOwner, address newOwner);
+
+
 }
 
 //  TODO's: Create a Pausable contract that inherits from the Ownable contract
